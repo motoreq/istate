b4331513be10e87b2bdbd4c36647968d9ba07540
Prasanth Sundaravelu <prasanths96@gmail.com>
2020-04-17 15:25:10 +0530

    Added Copyright text to files.

--- doc.go
+++ doc.go
@@ -1,4 +1,18 @@
-//
+/*
+	Copyright 2020 Prasanth Sundaravelu
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
 
 /*
 	Package iState is used to easily manage perform CRUD operations
@@ -68,8 +82,7 @@ import (
 
 // Debts:
 // Cleanup *stub - remove stub from all internal functions
-// Cleanup Errors when have time
-// Clean errors / error.go
+// Cleanup Errors when have time (error.go)
 // fetchCmplx and evalCmplx function is different from others, and needs extra info, try clean it up for symmetry
 
 // Encryption support?
