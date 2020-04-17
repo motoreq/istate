6991918ab7fa25d19c40f157c00d99f5f2ab8665
Prasanth Sundaravelu <prasanths96@gmail.com>
2020-04-17 15:54:30 +0530

    Added Copyright text to test files.

--- test/peer_exports.sh
+++ test/peer_exports.sh
@@ -1,3 +1,17 @@
+# Copyright 2020 Prasanth Sundaravelu
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 export CORE_VM_ENDPOINT=unix:///host/var/run/docker.sock
 export CORE_VM_DOCKER_HOSTCONFIG_NETWORKMODE=${COMPOSE_PROJECT_NAME}_fabricnetwork
 export CORE_LOGGING_LEVEL=DEBUG
